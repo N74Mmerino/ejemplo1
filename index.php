bdb55ff610333ff51fa64794af153f9d5f9a2161
N74Mmerino <nnicolasmmerino@gmail.com>
2024-04-12 21:02:04 -0300

    comint

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+$nombre = $_GET['n']; 
+$apellido = $_GET['a'];
+$anioNacimiento = 1985;
+$supuestaEdad = 2024 - $anioNacimiento; 
+
+echo 'Apellido: ' . $apellido . '. Nombre: ' . $nombre ; 
+echo '<br>';
+echo 'Nacio hace ' .$supuestaEdad. ' años';
+echo '<br>';
+echo $nombre . ' este es el nombre';
+
+
+
+    
